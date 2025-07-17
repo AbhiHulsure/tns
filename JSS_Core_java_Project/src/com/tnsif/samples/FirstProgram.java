f12e59c15eea094db689f4e3781b7b9744b327dc
Abhishek Hulsure <1js22cs006@jssateb.ac.in>
2025-07-17 18:14:27 +0530

    commit

--- JSS_Core_java_Project/src/com/tnsif/samples/FirstProgram.java
+++ JSS_Core_java_Project/src/com/tnsif/samples/FirstProgram.java
@@ -9,9 +9,7 @@ package com.tnsif.samples;
 public class FirstProgram {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-        System.out.println("Welcome to java learning");
-        System.out.println("Hello World");
-        System.out.println("Welcome to java learning");
+        
 	}
 
 }
