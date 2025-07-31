38858ec7e885f607a210c2af4a9ab0ab4ccbac1a
Abhishek Hulsure <1js22cs006@jssateb.ac.in>
2025-07-31 19:08:54 +0530

    1st commit

---
+++ JSS_Core_java_Project/src/com/tnsif/inheritance/SLInheritanceDemo.java
@@ -0,0 +1,10 @@
+package com.tnsif.inheritance;
+
+public class SLInheritanceDemo {
+        
+	public static void main(String args[]) {
+		Student s1=new Student("Abhi","Bangalore",12345688907L,1234567890,123,"JSS");
+		System.out.println(s1);
+		
+	}
+}
